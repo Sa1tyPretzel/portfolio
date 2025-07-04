4d823f24ea3f016a84ef66acb48ea5e520bedbf3
Sa1tyPretzel <mithileshpawar1@gmail.com>
2025-07-05 02:40:52 +0530

    UI zoom

--- src/ReactUI.jsx
+++ src/ReactUI.jsx
@@ -1,8 +1,10 @@
+import CameraController from "./reactComponents/CameraController";
+
 export default function ReactUI() {
     return(
         <>
-            <p>Click To Move</p>
-            
+            <p className="controls-message">Click To Move</p>
+            <CameraController />
         </>
     );
 }
