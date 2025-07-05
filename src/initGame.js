5ce1c2cdd5a0e7eff43a91f6214cc07775ca7479
Sa1tyPretzel <mithileshpawar1@gmail.com>
2025-07-06 03:10:17 +0530

    skills section working

--- src/initGame.js
+++ src/initGame.js
@@ -5,10 +5,12 @@ import { cameraZoomValueAtom, store } from "./store";
 import makeSection from "./components/Section";
 import makeEmailIcon from "./components/EmailIcon";
 import makeSocialIcon from "./components/SocialIcon";
+import makeSkillIcon from "./components/SkillIcon";
 import { makeAppear } from "./utils";
 
 export default async function initGame() {
     const generalData = await (await fetch("./configs/generalData.json")).json();
+    const skillsData = await (await fetch("./configs/skillsData.json")).json();
     const socialsData = await (await fetch("./configs/socialsData.json")).json();
 
     const k = makeKaplayCtx();
@@ -143,9 +145,30 @@ export default async function initGame() {
       }
     );
 
-    makeSection(k, k.vec2(k.center().x - 400, k.center().y), "Skills", (parent) => {
-        
-    });
+    makeSection(
+      k,
+      k.vec2(k.center().x - 400, k.center().y),
+      generalData.section2Name,
+      (parent) => {
+      
+        const container = parent.add([
+          k.opacity(0),
+          k.pos(-300, 0),
+        ]);
+
+        for (const skillData of skillsData) {
+          makeSkillIcon(
+            k,
+            container,
+            k.vec2(skillData.pos.x, skillData.pos.y),
+            skillData.logoData,
+            skillData.name
+          );
+        }  
+
+        makeAppear(k, container);
+        }
+    );
 
     makeSection(k, k.vec2(k.center().x + 400, k.center().y), "Experience", (parent) => {
         
